b51a392dd6e1b771fc585b6366e8a2f85c4829f9
Vincent <jliu7095@uni.sydney.edu.au>
2024-02-22 06:13:48 +1100

    python backend task completed

--- backend/py/task.py
+++ backend/py/task.py
@@ -13,21 +13,73 @@ class File:
 Task 1
 """
 def leafFiles(files: list[File]) -> list[str]:
-    return []
+    files_dict = {file.id:file.name for file in files}
+    name_files = [file.name for file in files]
+
+    # Remove the Parent file from pool
+    for file in files:
+        if file.parent != -1 and files_dict[file.parent] in name_files:  
+            name_files.remove(files_dict[file.parent])
+
+    return name_files
 
 
 """
 Task 2
 """
 def kLargestCategories(files: list[File], k: int) -> list[str]:
-    return []
+    # the dict [category_name, count]
+    categories_list = {}
+    for file in files:
+        for category in file.categories:
+            if category in categories_list.keys():
+                categories_list[category] += 1
+            else:
+                categories_list[category] = 1
+
+    sorted_category_list = sorted(sorted(categories_list.items(), key = lambda x : x[0]), key = lambda x : x[1],reverse=True)  
+    sorted_category_list = [category[0] for category in sorted_category_list]
+    
+    if len(sorted_category_list) < k:
+        return sorted_category_list
+    else: 
+        return sorted_category_list[:k]
 
 
 """
 Task 3
 """
 def largestFileSize(files: list[File]) -> int:
-    return 0
+    road_map = []
+    files_name_dict = {file.name: file for file in files}
+    files_id_dict = {file.id: file for file in files}
+    leaf_files = [files_name_dict[name] for name in leafFiles(files)]
+    
+    # Find Every path to the root from the leaf nodes
+    for file in leaf_files:
+        pointer = file
+        road = [pointer.name]
+        while pointer.parent != -1:
+            pointer = files_id_dict[pointer.parent]
+            road.append(pointer.name)
+        if road != []: road_map.append(road)        
+    
+    # Merge paths containing the same root
+    merged_roads = {}
+    for road in road_map:
+        if road[-1] not in merged_roads.keys():
+            merged_roads[road[-1]] = road
+        else:
+            merged_roads[road[-1]] = list(set(merged_roads[road[-1]]) | set(road[:-1]))
+
+    # Count the size of every path
+    max_num = 0
+    for road in merged_roads.values():
+        num = sum(files_name_dict[pointer].size for pointer in road)
+        if max_num < num: 
+            max_num = num 
+
+    return max_num
 
 
 if __name__ == '__main__':
